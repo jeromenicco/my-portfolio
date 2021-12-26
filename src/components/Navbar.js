c37cc424b522741ea8980dd98e8a73e71b33b72b
Jerome Nicco <jeromenicco@MacBook-Air-de-Jerome.local>
2021-12-26 12:33:28 +0000

    test fix issue show/hide -> window.scrollY > 60 -> NavBar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
     // console.log(lastScrollY)
     
     window.addEventListener('scroll', () => {
-        if (lastScrollY < window.scrollY || window.scrollY > 60) {
+        if (lastScrollY < window.scrollY && window.scrollY > 60) {
             setScrollDown(true)
         }
         else {
