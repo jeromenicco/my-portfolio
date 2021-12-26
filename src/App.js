77391fff958695f45541c27ef6d31eaa974471a5
Jerome Nicco <jeromenicco@MacBook-Air-de-Jerome.local>
2021-12-26 12:00:29 +0000

    test fix issue show/hide -> change scrollTop to 1 and scrollBottom to -1  -> App.css

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
-
+import React, {useEffect} from 'react'
 import Navbar from './components/Navbar'
 import About from './components/About'
 import Work from './components/Work';
 import Contact from './components/Contact';
 
 import './App.css';
-import { useEffect } from 'react/cjs/react.development';
 
 function App() {
-
+  
   const htmlElement = document.querySelector('body')
 
-  console.log(htmlElement)
+  useEffect(() => {
+    afterRender()
+    return () => {
+      beforeRemove()
+    }
+  })
 
 
   const scrollToPreventBounce = (htmlElement) => {
     const {scrollTop, offsetHeight, scrollHeight} = htmlElement;
+      // If at top, bump down 1px
       if (scrollTop <= 0) {
         htmlElement.scrollTo(0, 1);
         return;
       }
+      // If at bottom, bump up 1px
       if (scrollTop + offsetHeight >= scrollHeight) {
         htmlElement.scrollTo(0, scrollHeight - offsetHeight - 1);
       }
   }
+  // When rendering the element
+  function afterRender() {
+     htmlElement.addEventListener('touchstart', scrollToPreventBounce);
+  }
+  // Remember to clean-up when removing it
+  function beforeRemove() {
+     htmlElement.removeEventListener('touchstart', scrollToPreventBounce);
+  }
 
-  useEffect(() => {
-    htmlElement.addEventListener('touchstart', scrollToPreventBounce);
-    return () => htmlElement.removeEventListener('touchstart', scrollToPreventBounce);
-  })
 
   
   return (
